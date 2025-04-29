aaab6900cd5fa7b3b6c5241a095c29ab83a7f0dc
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-29 10:47:11 -0300

    feat: implementation without redux

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,16 +8,21 @@ import Soma from './components/Soma';
 import Sorteio from './components/Sorteio';
 
 function App() {
+
+  const [min, setMin] = useState(10);
+  const [max, setMax] = useState(20);
+
   return (
     <div className="App">
       <h1>Exercicio react-redux (simples)</h1>
       <div className='linha'>
-        <Intervalo></Intervalo>
+        <Intervalo min={min} max={max}
+        onMinChanged={setMin} onMaxChanged={setMax}></Intervalo>
       </div>
       <div className='linha'>
-        <Media></Media>
-        <Soma></Soma>
-        <Sorteio></Sorteio>
+        <Media min={min} max={max}></Media>
+        <Soma min={min} max={max}></Soma>
+        <Sorteio min={min} max={max}></Sorteio>
       </div>
     </div>
   );
